a39f5a7db45e9f8374c1d984b8405f595f8b60bc
k2645 <k2645@naver.com>
2023-04-14 16:22:48 +0900

    [style] 로그인 화면 구현

--- Dot_Order/Dot_Order/Controllers/LoginViewController.swift
+++ Dot_Order/Dot_Order/Controllers/LoginViewController.swift
@@ -9,14 +9,31 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var idView: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginButton.layer.cornerRadius = 20
+        
+        idView.layer.cornerRadius = 15
+        idView.layer.borderWidth = 3
+        idView.layer.borderColor = UIColor(named: "main_color")?.cgColor
+        passwordView.layer.cornerRadius = 15
+        passwordView.layer.borderWidth = 3
+        passwordView.layer.borderColor = UIColor(named: "main_color")?.cgColor
+        
+        passwordTextField.isSecureTextEntry = true
         
     }
-
-    // MARK: 카카오 로그인 버튼
-    @IBAction func kakaoLoginButton(_ sender: Any) {
+    
+    
+    // MARK: 로그인 버튼
+    @IBAction func loginButtonTapped(_ sender: Any) {
         guard let mainViewController = self.storyboard?.instantiateViewController(withIdentifier: "MainNC") else { return }
         mainViewController.modalPresentationStyle = .fullScreen
         self.present(mainViewController, animated: true, completion: nil)
